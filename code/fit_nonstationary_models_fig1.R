4db5122bd73a67d3594ee6441fb6bc44cd909beb
eric-ward <eric.ward@noaa.gov>
2020-12-16 16:01:09 -0800

    rejigger these plots

--- code/fit_nonstationary_models_fig1.R
+++ code/fit_nonstationary_models_fig1.R
@@ -2,6 +2,7 @@ library(sdmTMB)
 library(sp)
 library(ggplot2)
 library(ggsidekick)
+library(dplyr)
 species = read.csv("survey_data/species_list.csv")
 bio = readRDS("survey_data/wcbts_bio_2019-08-01.rds")
 haul = readRDS("survey_data/wcbts_haul_2019-08-01.rds")
@@ -26,25 +27,34 @@ dat$lat = dat$latitude_dd/1000
 dat = dplyr::filter(dat, scientific_name %in% c("Eopsetta jordani",
   "Sebastolobus altivelis","Sebastes crameri","Sebastes jordani"))
 
+# set up df of cells surveyed. all we really care about are epsilon_st by year, so depth can be 0
+unique_coords = strsplit(unique(paste(floor(dat$lon), floor(dat$lat))), " ")
+grid_dat = expand.grid(depth_scaled = 0, depth_scaled2 = 0,
+  year = unique(dat$year),
+  i = seq(1, length(unique_coords)))
+grid_dat$lon = as.numeric(unlist(lapply(unique_coords, getElement, 1)))[grid_dat$i]
+grid_dat$lat = as.numeric(unlist(lapply(unique_coords, getElement, 2)))[grid_dat$i]
+
 models = list()
 for(i in 1:length(unique(dat$scientific_name))) {
-  
-  sub = dplyr::filter(dat, scientific_name==unique(dat$scientific_name)[i]) %>% 
+
+  sub = dplyr::filter(dat, scientific_name==unique(dat$scientific_name)[i]) %>%
     dplyr::filter(cpue_kg_km2>0)
-  spde <- make_mesh(sub, c("lon", "lat"), cutoff = 10) 
+  spde <- make_mesh(sub, c("lon", "lat"), cutoff = 10)
   sub$depth_scaled = as.numeric(scale(sub$depth_m))
   sub$depth_scaled2 = sub$depth_scaled^2
 
   m = sdmTMB(cpue_kg_km2 ~ 0 + depth_scaled + depth_scaled2 + as.factor(year),
     data = sub, time = "year", spde = spde, family = Gamma(link = "log"))
   models[[i]] = m
-  
-  # calculate residuals 
-  sub$resid = sub$cpue_kg_km2 - predict(m)$est
+
+  # calculate residuals
+  #sub$resid = sub$cpue_kg_km2 - predict(m)$est
+  grid_dat$resid = predict(models[[i]], newdata=grid_dat)$epsilon_st
   # bootstrap residuals by year to calculate sd broom::bootstrap() deprecated
   sp_df = data.frame("species"=sub$scientific_name[1], "year"=unique(dat$year), "low"=NA, "mean"=NA, "hi"=NA)
   for(y in 1:length(unique(dat$year))) {
-    sub_yr = dplyr::filter(sub, year==unique(dat$year)[y]) %>% dplyr::select(resid)
+    sub_yr = dplyr::filter(grid_dat, year==unique(dat$year)[y]) %>% dplyr::select(resid)
     m = matrix(sample(sub_yr$resid, size = length(sub_yr$resid)*1000, replace=TRUE), 1000, length(sub_yr$resid))
     sds = apply(m,1,sd)
     sp_df$low[y] = quantile(log(sds),0.025)
@@ -67,18 +77,18 @@ df$common_name[which(df$species=="Sebastes jordani")] = "Shortbelly rockfish"
 
 
 g1 = df %>%
-ggplot(aes(year, mean)) + 
-  geom_ribbon(aes(ymin=low,ymax=hi),alpha=0.3,fill="darkblue") + 
-  geom_point(aes(year,mean),col="darkblue",size=2) + 
-  geom_line(col="darkblue") + 
-  facet_wrap(~common_name,scale="free_y") + 
-  theme_sleek() + 
+ggplot(aes(year, mean)) +
+  geom_ribbon(aes(ymin=low,ymax=hi),alpha=0.3,fill="darkblue") +
+  geom_point(aes(year,mean),col="darkblue",size=2) +
+  geom_line(col="darkblue") +
+  facet_wrap(~common_name,scale="free_y") +
+  theme_sleek() +
   xlab("Year") + ylab(expression(paste("Ln(",sigma,") of residuals")))
 
-pdf("plots/figure_1.pdf") 
+pdf("plots/figure_S1.pdf")
 g1
 dev.off()
 
-jpeg("plots/figure_1.jpeg") 
+jpeg("plots/figure_S1.jpeg")
 g1
 dev.off()
