cf0718d98f7c4644b6c774613abcf4616eaf5f8f
Lewis Barnett <labarnett@ucdavis.edu>
2020-12-23 16:00:18 -0800

    reduce knots and include all models

--- code/fit_nonstationary_models_all_data_re.R
+++ code/fit_nonstationary_models_all_data_re.R
@@ -1,6 +1,6 @@
 ## Fit Stationary and Non-Stationary Models to zero and positive responses ##
 
-#remotes::install_github("pbs-assess/sdmTMB", ref = "epsilon-heterogeneity")
+remotes::install_github("pbs-assess/sdmTMB", ref = "epsilon-heterogeneity")
 library(sdmTMB)
 library(dplyr)
 
@@ -21,47 +21,80 @@ for(i in 1:nrow(species)){
   dat$depth_scaled = as.numeric(scale(dat$depth_m))
   dat$depth_scaled2 = dat$depth_scaled^2
 
-  spde = make_mesh(dat, c("lon", "lat"), cutoff = 10)
+  spde = make_mesh(dat, c("lon", "lat"), cutoff = 25) # 20 -> 389 knots; 25 -> 294 knots; 30 -> 221 knots
 
-  # adults and juveniles combined
-  total_fit_stationary_re = try(sdmTMB(cpue_kg_km2 ~ 0 + depth_scaled + depth_scaled2 + as.factor(year),
+  # stationary fits
+  m_total = try(sdmTMB(cpue_kg_km2 ~ 0 + depth_scaled + depth_scaled2 + as.factor(year),
+                       data = dat, time = "year", spde = spde, family = tweedie(link = "log"),
+                       nlminb_loops = 2, newton_steps = 1)
+  )
+  m_juv = try(sdmTMB(juv_cpue_kg_km2 ~ 0 + depth_scaled + depth_scaled2 + as.factor(year),
+                     data = dat, time = "year", spde = spde, family = tweedie(link = "log"),
+                     nlminb_loops = 2, newton_steps = 1)
+  )
+  m_adult = try(sdmTMB(adult_cpue_kg_km2 ~ 0 + depth_scaled + depth_scaled2 + as.factor(year),
+                       data = dat, time = "year", spde = spde, family = tweedie(link = "log"),
+                       nlminb_loops = 2, newton_steps = 1)
+  )
+
+  print(paste0("species ", i, " of ", nrow(species), " stationary complete"))
+
+  # with loglinear on sigma_epsilon
+  m_total_ll = try(sdmTMB(cpue_kg_km2 ~ 0 + depth_scaled + depth_scaled2 + as.factor(year),
+                         data = dat, time = "year", spde = spde, family = tweedie(link = "log"),
+                         nlminb_loops = 2, newton_steps = 1,
+                         epsilon_model = "loglinear")
+  )
+  m_juv_ll = try(sdmTMB(juv_cpue_kg_km2 ~ 0 + depth_scaled + depth_scaled2 + as.factor(year),
+                       data = dat, time = "year", spde = spde, family = tweedie(link = "log"),
+                       nlminb_loops = 2, newton_steps = 1,
+                       epsilon_model = "loglinear")
+  )
+  m_adult_ll = try(sdmTMB(adult_cpue_kg_km2 ~ 0 + depth_scaled + depth_scaled2 + as.factor(year),
+                      data = dat, time = "year", spde = spde, family = tweedie(link = "log"),
+                      nlminb_loops = 2, newton_steps = 1,
+                      epsilon_model = "loglinear")
+  )
+
+  print(paste0("species ", i, " of ", nrow(species), " loglinear complete"))
+
+  # with random effects on sigma_epsilon
+  m_total_re = try(sdmTMB(cpue_kg_km2 ~ 0 + depth_scaled + depth_scaled2 + as.factor(year),
                        data = dat, time = "year", spde = spde, family = tweedie(link = "log"),
                        nlminb_loops = 2, newton_steps = 1,
                        epsilon_model = "re")
                   )
-  # juveniles
-  juv_fit_stationary_re = try(sdmTMB(juv_cpue_kg_km2 ~ 0 + depth_scaled + depth_scaled2 + as.factor(year),
+  m_juv_re = try(sdmTMB(juv_cpue_kg_km2 ~ 0 + depth_scaled + depth_scaled2 + as.factor(year),
                      data = dat, time = "year", spde = spde, family = tweedie(link = "log"),
                      nlminb_loops = 2, newton_steps = 1,
                      epsilon_model = "re")
                 )
-  # adults
-  ad_fit_stationary_re = try(sdmTMB(adult_cpue_kg_km2 ~ 0 + depth_scaled + depth_scaled2 + as.factor(year),
+  m_adult_re = try(sdmTMB(adult_cpue_kg_km2 ~ 0 + depth_scaled + depth_scaled2 + as.factor(year),
                    data = dat, time = "year", spde = spde, family = tweedie(link = "log"),
                    nlminb_loops = 2, newton_steps = 1,
                    epsilon_model = "re")
                )
 
-  print(paste0("species ", i, " of ", nrow(species), " halfway complete"))
+  print(paste0("species ", i, " of ", nrow(species), " re complete"))
 
-  # same as the above 3 models but using a combination of
-  total_fit_re = try(sdmTMB(cpue_kg_km2 ~ 0 + depth_scaled + depth_scaled2 + as.factor(year),
+  # with combination of loglinear and random effects on sigma_epsilon
+  m_total_ll_re = try(sdmTMB(cpue_kg_km2 ~ 0 + depth_scaled + depth_scaled2 + as.factor(year),
                          data = dat, time = "year", spde = spde, family = tweedie(link = "log"),
                          nlminb_loops = 2, newton_steps = 1,
                          epsilon_model = "loglinear-re")
   )
-  juv_fit_re = try(sdmTMB(juv_cpue_kg_km2 ~ 0 + depth_scaled + depth_scaled2 + as.factor(year),
+  m_juv_ll_re = try(sdmTMB(juv_cpue_kg_km2 ~ 0 + depth_scaled + depth_scaled2 + as.factor(year),
                        data = dat, time = "year", spde = spde, family = tweedie(link = "log"),
                        nlminb_loops = 2, newton_steps = 1,
                        epsilon_model = "loglinear-re")
   )
-  ad_fit_re = try(sdmTMB(adult_cpue_kg_km2 ~ 0 + depth_scaled + depth_scaled2 + as.factor(year),
+  m_adult_ll_re = try(sdmTMB(adult_cpue_kg_km2 ~ 0 + depth_scaled + depth_scaled2 + as.factor(year),
                       data = dat, time = "year", spde = spde, family = tweedie(link = "log"),
                       nlminb_loops = 2, newton_steps = 1,
                       epsilon_model = "loglinear-re")
   )
 
-  rm(dat)
+  rm(dat, spde)
   if (!dir.exists("output")) {dir.create("output")}
-  save.image(file=paste0("output/", sub(" ", "_", comm_name),"_all_models_re.RData"))
+  save.image(file=paste0("output/", sub(" ", "_", comm_name),"_all_models.RData"))
 }
