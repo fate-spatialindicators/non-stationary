e84c7e91048f680847532c8a7646c06cb3f87b31
Eric Ward <eric.ward@noaa.gov>
2021-01-29 09:50:30 -0800

    commit delta output

--- code/fit_nonstationary_models_total_cpue.R
+++ code/fit_nonstationary_models_total_cpue.R
@@ -39,3 +39,61 @@ for(i in 1:nrow(species)){
   if (!dir.exists("output")) {dir.create("output")}
   save.image(file=paste0("output/", sub(" ", "_", comm_name),"_all_models.RData"))
 }
+
+# fit presence - absence models
+for(i in 1:nrow(species)){
+
+  comm_name = species$common_name[i]
+
+  sub = dplyr::filter(dat, scientific_name == species$scientific_name[i])
+
+  sub$depth_scaled = as.numeric(scale(sub$depth_m))
+  sub$depth_scaled2 = sub$depth_scaled^2
+
+  spde = make_mesh(sub, c("lon", "lat"), cutoff = 10)
+
+  sub$presence = ifelse(sub$cpue_kg_km2 > 0, 1, 0)
+  # total cpue
+  ad_fit = try(sdmTMB(presence ~ 0 + depth_scaled + depth_scaled2 + as.factor(year),
+                      data = sub, time = "year", spde = spde, family = binomial(link = "logit"))
+  )
+
+  sub$time = sub$year - min(sub$year) + 1
+  ad_fit_ll = try(sdmTMB(presence ~ 0 + depth_scaled + depth_scaled2 + as.factor(year),
+                         data = sub, time = "year", spde = spde, family = binomial(link = "logit"),
+                         epsilon_predictor = "time")
+  )
+
+  if (!dir.exists("output")) {dir.create("output")}
+  save.image(file=paste0("output/", sub(" ", "_", comm_name),"_presence_models.RData"))
+}
+
+
+# fit positive models
+for(i in 1:nrow(species)){
+
+  comm_name = species$common_name[i]
+
+  sub = dplyr::filter(dat, scientific_name == species$scientific_name[i])
+
+  sub$depth_scaled = as.numeric(scale(sub$depth_m))
+  sub$depth_scaled2 = sub$depth_scaled^2
+
+  sub = dplyr::filter(sub, cpue_kg_km2 > 0)
+  spde = make_mesh(sub, c("lon", "lat"), cutoff = 10)
+
+  #sub$presence = ifelse(sub$cpue_kg_km2 > 0, 1, 0)
+  # total cpue
+  ad_fit = try(sdmTMB(cpue_kg_km2 ~ 0 + depth_scaled + depth_scaled2 + as.factor(year),
+                      data = sub, time = "year", spde = spde, family = Gamma(link = "log"))
+  )
+
+  sub$time = sub$year - min(sub$year) + 1
+  ad_fit_ll = try(sdmTMB(cpue_kg_km2 ~ 0 + depth_scaled + depth_scaled2 + as.factor(year),
+                         data = sub, time = "year", spde = spde, family = Gamma(link = "log"),
+                         epsilon_predictor = "time")
+  )
+
+  if (!dir.exists("output")) {dir.create("output")}
+  save.image(file=paste0("output/", sub(" ", "_", comm_name),"_positive_models.RData"))
+}
--- code/process_data.R
+++ code/process_data.R
@@ -130,3 +130,114 @@ p4 = dplyr::filter(species_df,
   theme_bw() +
   scale_color_viridis(discrete=TRUE,end=0.8)
 
+
+
+
+# Presence absence models
+species = read.csv("survey_data/species_list.csv")
+names(species) = tolower(names(species))
+species = dplyr::rename(species,
+                        common_name = common.name,
+                        scientific_name = scientific.name)
+
+for(i in 1:nrow(species)){
+  print(i)
+  comm_name = species$common_name[i]
+  load(file=paste0("output/", sub(" ", "_", comm_name),"_presence_models.RData"))
+
+  df = data.frame(name = comm_name,
+                  model = c("adult", "adult"),
+                  loglinear = c(FALSE,TRUE))
+  df$aic = NA
+  df$trend = NA
+  df$trend_se = NA
+
+  if(class(ad_fit)!="try-error") {
+    df$aic[1] = AIC(ad_fit)
+  }
+  if(class(ad_fit_ll)!="try-error") {
+    df$aic[2] = AIC(ad_fit_ll)
+    df$trend[2] = ad_fit_ll$sd_report$value[which(names(ad_fit_ll$sd_report$value) == "b_epsilon_logit")]
+    df$trend_se[2] = ad_fit_ll$sd_report$sd[which(names(ad_fit_ll$sd_report$value) == "b_epsilon_logit")]
+  }
+  if(i==1) {
+    df_all = df
+  } else {
+    df_all = rbind(df_all, df)
+  }
+}
+
+# plot trend coefficient for presence-absence models
+p3 = dplyr::filter(df_all, loglinear==TRUE) %>%
+  ggplot(aes(name,trend)) +
+  geom_pointrange(aes(ymin=trend-2*trend_se, ymax=trend+2*trend_se),size=0.8,alpha=0.8,
+                  position = position_dodge(width = 0.9)) +
+  xlab("Species") +
+  ylab("Trend in total presence-absence spatiotemporal sd (+/- 2SE)") +
+  geom_hline(aes(yintercept=0),col="red",alpha=0.6) +
+  ylim(-5,5) +
+  coord_flip() +
+  theme_bw() +
+  scale_color_viridis(discrete=TRUE,end=0.8)
+
+aic_summary = dplyr::group_by(df_all, name, model) %>%
+  dplyr::summarize(min_aic = min(aic,na.rm=T),
+                   diff_aic = aic[which(loglinear==TRUE)] - min_aic) %>%
+  dplyr::arrange(model, name) %>%
+  as.data.frame()
+write.csv(aic_summary, "output/aic_summary_presence.csv")
+
+
+# Positivie models
+species = read.csv("survey_data/species_list.csv")
+names(species) = tolower(names(species))
+species = dplyr::rename(species,
+                        common_name = common.name,
+                        scientific_name = scientific.name)
+
+for(i in 1:nrow(species)){
+  print(i)
+  comm_name = species$common_name[i]
+  load(file=paste0("output/", sub(" ", "_", comm_name),"_positive_models.RData"))
+
+  df = data.frame(name = comm_name,
+                  model = c("adult", "adult"),
+                  loglinear = c(FALSE,TRUE))
+  df$aic = NA
+  df$trend = NA
+  df$trend_se = NA
+
+  if(class(ad_fit)!="try-error") {
+    df$aic[1] = AIC(ad_fit)
+  }
+  if(class(ad_fit_ll)!="try-error") {
+    df$aic[2] = AIC(ad_fit_ll)
+    df$trend[2] = ad_fit_ll$sd_report$value[which(names(ad_fit_ll$sd_report$value) == "b_epsilon_logit")]
+    df$trend_se[2] = ad_fit_ll$sd_report$sd[which(names(ad_fit_ll$sd_report$value) == "b_epsilon_logit")]
+  }
+  if(i==1) {
+    df_all = df
+  } else {
+    df_all = rbind(df_all, df)
+  }
+}
+
+# plot trend coefficient for presence-absence models
+p3 = dplyr::filter(df_all, loglinear==TRUE) %>%
+  ggplot(aes(name,trend)) +
+  geom_pointrange(aes(ymin=trend-2*trend_se, ymax=trend+2*trend_se),size=0.8,alpha=0.8,
+                  position = position_dodge(width = 0.9)) +
+  xlab("Species") +
+  ylab("Trend in total positive spatiotemporal sd (+/- 2SE)") +
+  geom_hline(aes(yintercept=0),col="red",alpha=0.6) +
+  ylim(-5,5) +
+  coord_flip() +
+  theme_bw() +
+  scale_color_viridis(discrete=TRUE,end=0.8)
+
+aic_summary = dplyr::group_by(df_all, name, model) %>%
+  dplyr::summarize(min_aic = min(aic,na.rm=T),
+                   diff_aic = aic[which(loglinear==TRUE)] - min_aic) %>%
+  dplyr::arrange(model, name) %>%
+  as.data.frame()
+write.csv(aic_summary, "output/aic_summary_positive.csv")
