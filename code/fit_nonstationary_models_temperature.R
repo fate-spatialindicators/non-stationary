ffb5ccaea210136b1c00207898663444818a710d
Eric Ward <eric.ward@noaa.gov>
2021-01-30 15:31:42 -0800

    debug

--- code/fit_nonstationary_models_temperature.R
+++ code/fit_nonstationary_models_temperature.R
@@ -24,17 +24,28 @@ sub = dplyr::filter(dat,
 sub$depth_scaled = as.numeric(scale(sub$depth_m))
 sub$depth_scaled2 = sub$depth_scaled^2
 
+# create calendar day
+sub$month = as.numeric(substr(sub$date_yyyymmdd, 5, 6))
+sub$day = as.numeric(substr(sub$date_yyyymmdd, 7, 8))
+sub$Date = lubridate::parse_date_time(paste(sub$year, sub$month, sub$day), orders = "ymd")
+sub$yday = lubridate::yday(sub$Date)
+
+sub$calday = as.numeric(scale(sub$yday))
+sub$calday2 = sub$calday^2
+
 sub = dplyr::filter(sub, !is.na(temperature_at_gear_c_der))
 spde = make_mesh(sub, c("lon", "lat"), cutoff = n_cutoff)
 
 # total cpue
-fit = try(sdmTMB(temperature_at_gear_c_der ~ 0 + depth_scaled + depth_scaled2 + as.factor(year),
+fit = try(sdmTMB(temperature_at_gear_c_der ~ 0 + depth_scaled + depth_scaled2 +
+                   calday + calday2 + as.factor(year),
                     data = sub, time = "year", spde = spde)
 )
 
 # scale time
 sub$time = sub$year - min(sub$year) + 1
-fit_ll = try(sdmTMB(temperature_at_gear_c_der ~ 0 + depth_scaled + depth_scaled2 + as.factor(year),
+fit_ll = try(sdmTMB(temperature_at_gear_c_der ~ 0 + depth_scaled + depth_scaled2 +
+                      calday + calday2 + as.factor(year),
                        data = sub, time = "year", spde = spde, epsilon_predictor = "time")
 )
 
@@ -42,7 +53,8 @@ temp = dplyr::group_by(sub, year) %>%
   dplyr::summarize(mean_temp = mean(temperature_at_gear_c_der,na.rm=T))
 temp$mean_temp = scale(temp$mean_temp)
 sub = dplyr::left_join(sub, temp)
-fit_ll_temp = try(sdmTMB(temperature_at_gear_c_der ~ 0 + depth_scaled + depth_scaled2 + as.factor(year),
+fit_ll_temp = try(sdmTMB(temperature_at_gear_c_der ~ 0 + depth_scaled + depth_scaled2 +
+                           calday + calday2 + as.factor(year),
                     data = sub, time = "mean_temp", spde = spde, epsilon_predictor = "time")
 )
 # drop out high memory objects
