7a0ac7cd6e76d92c9ad98559361b086fe595717a
Eric Ward <eric.ward@noaa.gov>
2021-09-17 08:00:51 -0700

    update code to run with new priors and sdmControl list()

--- code/fit_nonstationary_models_total_cpue_parallel.R
+++ code/fit_nonstationary_models_total_cpue_parallel.R
@@ -1,12 +1,12 @@
 ## Fit Stationary and Non-Stationary Models to zero and positive responses ##
 ## In parallel ##
 
-remotes::install_github("pbs-assess/sdmTMB@pc-prior")
+#remotes::install_github("pbs-assess/sdmTMB@priors-experimental")
 library(sdmTMB)
 library(dplyr)
 library(future)
 options(future.globals.maxSize = 4e3 * 1024 ^ 2) # ~4GB
-plan(multisession, workers = max(floor(availableCores() - 1), 4L)) # or just specify # cores manually
+plan(multisession, workers = 8) # or just specify # cores manually
 if (!dir.exists("output")) dir.create("output")
 
 # 15 -> ~ 600 knots; 20 -> 389 knots; 25 -> 294 knots; 30 -> 221 knots
@@ -14,8 +14,8 @@ n_cutoff <- 20
 species <- read.csv("survey_data/species_list.csv", fileEncoding="UTF-8-BOM")
 names(species) <- tolower(names(species))
 species <- dplyr::rename(species,
-  common_name = common.name,
-  scientific_name = scientific.name
+                         common_name = common.name,
+                         scientific_name = scientific.name
 )
 
 dat <- readRDS("survey_data/all_data.rds")
@@ -33,63 +33,69 @@ dat$time <- as.numeric(dat$year) - floor(mean(unique(as.numeric(dat$year))))
 
 # spatial + spatiotemporal:
 fit_models <- function(sub) {
-  spde <- make_mesh(sub, c("lon", "lat"), cutoff = n_cutoff)
+  spde <- make_mesh(sub, c("lon", "lat"), cutoff = n_cutoff, type = "cutoff")
   ad_fit <- tryCatch({
     sdmTMB(cpue_kg_km2 ~ 0 + depth_scaled + depth_scaled2 + year,
-      data = sub, time = "year", spde = spde, family = tweedie(link = "log"),
-      nlminb_loops = 2, newton_steps = 1
-    )
-  }, error = function(e) NA)
-  ad_fit <- refit_model_if_needed(ad_fit)
+           data = sub, time = "year", spde = spde, family = tweedie(link = "log"),
+           control = sdmTMBcontrol(nlminb_loops = 2, newton_loops = 1)
+    )}, error = function(e) NA)
+  #ad_fit <- refit_model_if_needed(ad_fit)
   ad_fit_ll <- tryCatch({
     sdmTMB(cpue_kg_km2 ~ 0 + depth_scaled + depth_scaled2 + year,
-      data = sub, time = "year", spde = spde,
-      family = tweedie(link = "log"), epsilon_predictor = "time",
-      nlminb_loops = 2, newton_steps = 1
+           data = sub, time = "year", spde = spde,
+           family = tweedie(link = "log"), epsilon_predictor = "time",
+           control = sdmTMBcontrol(nlminb_loops = 2, newton_loops = 1)
     )}, error = function(e) NA)
-  ad_fit_ll <- refit_model_if_needed(ad_fit_ll)
+  #ad_fit_ll <- refit_model_if_needed(ad_fit_ll)
   save(ad_fit, ad_fit_ll,
-    file = paste0("output/", sub(" ", "_", sub$common_name[[1]]), "_all_models.RData")
+       file = paste0("output/", sub(" ", "_", sub$common_name[[1]]), "_all_models.RData")
   )
 }
 
 #split(dat, dat$scientific_name) %>%
 #  furrr::future_walk(fit_models)
-  # purrr::walk(fit_models) # serial for testing
+# purrr::walk(fit_models) # serial for testing
 
 # AR1 spatiotemporal:
 fit_models_ar1 <- function(sub) {
-  spde <- make_mesh(sub, c("lon", "lat"), cutoff = n_cutoff)
+  spde <- make_mesh(sub, c("lon", "lat"), cutoff = n_cutoff, type = "cutoff")
   ad_fit <- tryCatch({
     sdmTMB(cpue_kg_km2 ~ 0 + depth_scaled + depth_scaled2 + year,
-      ar1_fields = TRUE, include_spatial = FALSE,
-      data = sub, time = "year", spde = spde, family = tweedie(link = "log"),
-      matern_prior_E = c(15, 0.05, 10, 0.05),
-      nlminb_loops = 2, newton_steps = 1
-    )
-  }, error = function(e) NA)
-  ad_fit <- refit_model_if_needed(ad_fit)
+           fields = "AR1", include_spatial = FALSE,
+           data = sub, time = "year", spde = spde, family = tweedie(link = "log"),
+           control = sdmTMBcontrol(nlminb_loops = 2, newton_loops = 1),
+           priors = sdmTMBpriors(
+             phi = halfnormal(0, 10),
+             tweedie_p = normal(1.5, 2),
+             ar1_rho = normal(0, 0.99),
+             matern_st = pc_matern(range_gt = 10, sigma_lt = 1))
+    )}, error = function(e) NA)
+  #ad_fit <- refit_model_if_needed(ad_fit)
   ad_fit_ll <- tryCatch({
     sdmTMB(cpue_kg_km2 ~ 0 + depth_scaled + depth_scaled2 + year,
-      data = sub, time = "year", spde = spde,
-      ar1_fields = TRUE, include_spatial = FALSE,
-      family = tweedie(link = "log"), epsilon_predictor = "time",
-      matern_prior_E = c(15, 0.05, 10, 0.05),
-      nlminb_loops = 2, newton_steps = 1
+           data = sub, time = "year", spde = spde,
+           fields = "AR1", include_spatial = FALSE,
+           family = tweedie(link = "log"), epsilon_predictor = "time",
+           control = sdmTMBcontrol(nlminb_loops = 2, newton_loops = 1),
+           priors = sdmTMBpriors(
+             phi = halfnormal(0, 10),
+             tweedie_p = normal(1.5, 2),
+             ar1_rho = normal(0, 0.99),
+             matern_st = pc_matern(range_gt = 10, sigma_lt = 1))
     )}, error = function(e) NA)
-  ad_fit_ll <- refit_model_if_needed(ad_fit_ll)
+  #ad_fit_ll <- refit_model_if_needed(ad_fit_ll)
   save(ad_fit, ad_fit_ll,
-    file = paste0("output/", sub(" ", "_", sub$common_name[[1]]), "_ar1_range15_sigma10.RData")
+       file = paste0("output/", sub(" ", "_", sub$common_name[[1]]), "_ar1_priors.RData")
   )
 }
 
 refit_model_if_needed <- function(m) {
-  if (!is.na(m[[1]])) {
-    if (max(abs(m$gradients)) > 0.01) {
+  if (class(m)!="try-error") {
+    if (max(m$gradients) > 0.01) {
       m <- tryCatch({
         sdmTMB::run_extra_optimization(m,
-          nlminb_loops = 1L,
-          newton_loops = 1L
+                                       nlminb_loops = 1L,
+                                       newton_steps = 1L
         )
       }, error = function(e) m)
     }
@@ -97,8 +103,9 @@ refit_model_if_needed <- function(m) {
   m
 }
 
+
 split(dat, dat$scientific_name) %>%
   furrr::future_walk(fit_models_ar1)
-  # purrr::walk(fit_models_ar1) # serial for testing
+# purrr::walk(fit_models_ar1) # serial for testing
 
 plan(sequential) # avoid RStudio crashes on Session Restart R
